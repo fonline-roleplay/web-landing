f1c5c3427697eb01f520bacebe65c0e1a56f1224
Erandelax <erandelax@gmail.com>
2021-12-02 23:07:36 +0200

    refactoring

--- assets/app.js
+++ assets/app.js
@@ -1,42 +1,66 @@
 const { Splide, EVENT_SCROLL } = require('@splidejs/splide');
 
-document.addEventListener('DOMContentLoaded', function() {
-    for (const el of document.querySelectorAll('.splide')) {
-        const slides = el.querySelectorAll('.splide__slide')
-        const splide = new Splide(el)
-        splide.on('pagination:mounted', function(data) {
-            const sidebar = document.createElement('DIV');
-            const splide = data.list.parentElement;
-            const pagination = data.list;
-            sidebar.classList.add('splide__sidebar')
-            data.list.classList.add('splide__button-grid');
-            data.items.forEach(function(item) {
-                const slide = slides[item.page];
-                const title = slide.dataset.title || (item.page + 1);
-                const icon = slide.dataset.icon;
-                if (icon) {
-                    item.button.innerHTML = `<div class='btn__header'><div class=title>${title}</div><img class=icon src="${icon}"></div>`;
-                } else {
-                    item.button.innerHTML = `<div class='btn__header'><div class=title>${title}</div></div>`;
-                }
-                item.button.parentElement.classList.add('btn')
-                item.button.parentElement.classList.add('btn--with-dot')
-                item.button.classList.add('btn--transparent')
-            });
-            splide.appendChild(sidebar);
-            for (const block of splide.querySelectorAll('.splide__before-pagination') || []) {
-                sidebar.appendChild(block)
-            }
-            sidebar.appendChild(pagination);
+const SimpleBar = require('simplebar');
 
-            for (const block of splide.querySelectorAll('.splide__after-pagination') || []) {
-                sidebar.appendChild(block)
+function initSlider(el, isVertical = false) {
+    const slides = el.querySelectorAll('.splide__slide')
+    const options = {
+        wheel: false,
+        height: '800px',
+    };
+    if (isVertical) {
+        options['direction'] = 'ttb';
+    }
+    const splide = new Splide(el, options)
+    splide.on('pagination:mounted', function(data) {
+        const splide = data.list.parentElement;
+        if (splide.classList.contains('--slider')) return;
+
+        const sidebar = document.createElement('DIV');
+        const pagination = data.list;
+        sidebar.classList.add('splide__sidebar')
+        data.list.classList.add('splide__button-grid');
+        data.items.forEach(function(item) {
+            const slide = slides[item.page];
+            const title = slide.dataset.title || (item.page + 1);
+            const icon = slide.dataset.icon;
+            if (icon) {
+                item.button.innerHTML = `<div class='btn__header'><div class=title>${title}</div><img class=icon src="${icon}"></div>`;
+            } else {
+                item.button.innerHTML = `<div class='btn__header'><div class=title>${title}</div></div>`;
             }
+            item.button.parentElement.classList.add('btn')
+            item.button.parentElement.classList.add('btn--with-dot')
+            item.button.classList.add('btn--transparent')
         });
-        splide.mount();
+        splide.appendChild(sidebar);
+        for (const block of splide.querySelectorAll('.splide__before-pagination') || []) {
+            sidebar.appendChild(block)
+        }
+        sidebar.appendChild(pagination);
+
+        for (const block of splide.querySelectorAll('.splide__after-pagination') || []) {
+            sidebar.appendChild(block)
+        }
+    });
+    splide.on('mounted', function(e) {
+        for (const cel of splide.root.querySelectorAll('.splide:not(.is-initialized)')) {
+            initSlider(cel, false);
+        }
+        /*
+        for (const slider of splide.querySelectorAll('.splide > .splide__track > .splide__list > .splide__slide > .splide')) {
+            initSlider(slider);
+        }*/
+    });
+    splide.mount();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    for (const el of document.querySelectorAll('.tool > .splide:not(.is-initialized)')) {
+        initSlider(el, true);
     }
 
-    for (const el of document.querySelectorAll('.splide__slide--video')) {
+    /*for (const el of document.querySelectorAll('.splide__slide--video')) {
         el.addEventListener('click', function(e) {
             const targetURL = el.dataset.url;
             for (const target of el.querySelectorAll('iframe')) {
@@ -48,5 +72,14 @@ document.addEventListener('DOMContentLoaded', function() {
             }
             el.classList.add('is-playing')
         })
+    }*/
+
+    /*Array.prototype.forEach.call(
+        document.querySelectorAll('.--with-scrollbar'),
+        el => new SimpleBar()
+    );*/
+
+    for (const scrollbar of document.querySelectorAll('.--with-scrollbar')) {
+        new SimpleBar(scrollbar)
     }
 });
--- build.js
+++ build.js
@@ -1,21 +1,23 @@
-const {TwingEnvironment, TwingLoaderArray} = require('twing');
+const { TwingEnvironment, TwingLoaderArray } = require('twing');
 const YAML = require('yaml')
-const {readFileSync, writeFileSync} = require('fs');
+const { readFileSync, writeFileSync } = require('fs');
 
 console.log('Building...');
 
 const fsIndex = readFileSync('index.twig', 'utf8');
+const fsElements = readFileSync('elements.twig', 'utf8');
 const fsContent = readFileSync('content.yaml', 'utf8');
 const ymlContent = YAML.parse(fsContent);
 console.log("Content:\n", fsContent);
 
 const loader = new TwingLoaderArray({
-    'index.twig': fsIndex
+    'index.twig': fsIndex,
+    'elements.twig': fsElements
 });
 
 const twing = new TwingEnvironment(loader);
 
 twing.render('index.twig', ymlContent).then((page) => {
-    writeFileSync('index.html', page);
+    writeFileSync('index.html', page /*.replace(/ +/g, ' ')*/ );
     console.log('Complete.');
 });
